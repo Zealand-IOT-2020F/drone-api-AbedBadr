19b252a5085526df2854b6b9481c55395f423881
AbxdHxcks <abedbadr1999@hotmail.com>
2020-03-20 12:28:16 +0100

    update
    Co-authored-by: NicolaiAA <Nicolai5aa@gmail.com>

--- Drone.py
+++ Drone.py
@@ -6,9 +6,65 @@ import time
 class Drone(object):
     """description of class"""
 
+    def __init__(self, ip, port):
+        self.TelloIp = ip
+        print("ip: " + ip)
+        self.TelloPort = port
+        # UDP Socket
+        self.Host = ''
+        self.HostPort = 9000
+        self.locaddr = (self.Host, self.HostPort)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tello_address = ('192.168.10.1', 8889)
+        self.sock.bind(self.locaddr)
+
+    def sendMessage(self,TelloMessage):
+       print("send message " + TelloMessage + " end ")
+       msg = TelloMessage.encode(encoding="utf-8")
+       sent = self.sock.sendto(msg,self.tello_address)
+       data, server = self.sock.recvfrom(1518)
+       print(data.decode(encoding="utf-8")) 
+       return "from sendmessage " + TelloMessage + " end "
+
+    def printinfo(self):
+        print("Hallo Drone at : " + self.TelloIp)
+
+    def connect(self):
+        print ("Connect")
+        result = self.sendMessage("command")
+        print(result)
 
     def takeOff(self):
-        print("takeOff")
-        #add the takeoff code
+        print("takeoff")
+        result = self.sendMessage(" takeoff")
 
-    
+    def land(self):
+        print("land")
+        result = self.sendMessage(" land")
+
+    def end(self):
+        print("end")
+        self.sock.close()
+
+    def cw(self,x):
+        print("cw")
+        result = self.sendMessage(" cw "+x)
+
+    def ccw(self,X):
+        print("ccw")
+        result = self.sendMessage(" ccw "+ X)
+
+    def battery(self):
+        result = self.sendMessage(" battery?")
+        return result
+
+    def up(self, x):
+        print("up")
+        result = self.sendMessage("up " + x)
+
+    def forward(self, x):
+        print("forward")
+        if (x > 20 and x < 500):
+            result = self.sendMessage("forward " + x)
+        else:
+            print("cm has to be between 20-500")
--- Main.py
+++ Main.py
@@ -1,2 +1,19 @@
+import sys
+import time
+import Drone
 
-#here you should interact with the drone
+drone1 = Drone.Drone("128.1.1.1", 8889)
+
+drone1.connect()
+
+drone1.printinfo()
+
+drone1.takeOff()
+#time.sleep(2)
+
+while (drone1.battery() != 10):
+    drone1.up(20)
+    drone1.forward(20)
+
+drone1.land()
+drone1.end()
